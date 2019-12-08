562bf956e8e7ded89c0b92c9b0172ccd7dd355ff
Alex Pestell <alex.pestell@gmail.com>
2019-12-08 01:14:39 +0100

    Day 1 Part 2

---
+++ 1/partTwo.py
@@ -0,0 +1,17 @@
+import math;
+
+sum = 0
+
+def calculate_fuel(mass):
+    return math.floor(int(mass) / 3) - 2
+
+with open('input.txt') as input:
+    for line in input:
+        module_fuel = calculate_fuel(line)
+        fuel_fuel = calculate_fuel(module_fuel)
+        while fuel_fuel > 0:
+            module_fuel += fuel_fuel
+            fuel_fuel = calculate_fuel(fuel_fuel)
+        sum += module_fuel
+
+print(sum)
